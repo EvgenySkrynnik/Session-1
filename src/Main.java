43f1a0eea5266ec3de033dd0e6a24f3e25e8bb90
EvgenySkrynnik <skrynnik2011@gmail.com>
2023-03-07 14:26:56 +0100

    Coursework

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        // возведение в квадрат числа 3
+        int a1 = 3;
+        int b1 = 2;
+        System.out.printf(" число 3 в квадрате равно %.0f \\n", Math.pow(a1, b1));
+        // возведение числа в квадрат 5
+        int a2 = 5;
+        int b2 = 2;
+        System.out.println(" число 5 в квадрате равно " + Math.pow(a2, b2));
+
+
+
+
+    }
+
+
+}
